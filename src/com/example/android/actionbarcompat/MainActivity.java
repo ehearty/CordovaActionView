919a079fa38fd3de588d05f0fe67506a2b5bdf63
Joe Bowser <bowserj@apache.org>
2012-07-20 14:28:11 -0700

    Removed the local.properties, added code to use intents

--- src/com/example/android/actionbarcompat/MainActivity.java
+++ src/com/example/android/actionbarcompat/MainActivity.java
@@ -188,12 +188,105 @@ public class MainActivity extends ActionBarActivity implements CordovaInterface{
         // This is a no-op.
     }
     
-    public void onDestroy()
-    {
+
+    @Override
+    /**
+     * Called when an activity you launched exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     *
+     * @param requestCode       The request code originally supplied to startActivityForResult(),
+     *                          allowing you to identify who this result came from.
+     * @param resultCode        The integer result code returned by the child activity through its setResult().
+     * @param data              An Intent, which can return result data to the caller (various data can be attached to Intent "extras").
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        IPlugin callback = this.activityResultCallback;
+        if (callback != null) {
+            callback.onActivityResult(requestCode, resultCode, intent);
+        }
+    }
+    
+    @Override
+    /**
+     * Called when the system is about to start resuming a previous activity.
+     */
+    protected void onPause() {
+        super.onPause();
+
+         // Send pause event to JavaScript
+        this.mainView.loadUrl("javascript:try{cordova.fireDocumentEvent('pause');}catch(e){console.log('exception firing pause event from native');};");
+
+        // Forward to plugins
+        if (this.mainView.pluginManager != null) {
+            this.mainView.pluginManager.onPause(true);
+        }
+    }
+
+    @Override
+    /**
+     * Called when the activity receives a new intent
+     **/
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        //Forward to plugins
+        if ((this.mainView != null) && (this.mainView.pluginManager != null)) {
+            this.mainView.pluginManager.onNewIntent(intent);
+        }
+    }
+
+    @Override
+    /**
+     * Called when the activity will start interacting with the user.
+     */
+    protected void onResume() {
+        super.onResume();
+
+       
+        if (this.mainView == null) {
+            return;
+        }
+
+        // Send resume event to JavaScript
+        this.mainView.loadUrl("javascript:try{cordova.fireDocumentEvent('resume');}catch(e){console.log('exception firing resume event from native');};");
+
+        // Forward to plugins
+        if (this.mainView.pluginManager != null) {
+            this.mainView.pluginManager.onResume(true);
+        }
+
+    }
+
+    @Override
+    /**
+     * The final call you receive before your activity is destroyed.
+     */
+    public void onDestroy() {
+        LOG.d(TAG, "onDestroy()");
         super.onDestroy();
         if (mainView.pluginManager != null) {
             mainView.pluginManager.onDestroy();
         }
+        
+        if (this.mainView != null) {
+
+            // Send destroy event to JavaScript
+            this.mainView.loadUrl("javascript:try{cordova.require('cordova/channel').onDestroy.fire();}catch(e){console.log('exception firing destroy event from native');};");
+
+            // Load blank page so that JavaScript onunload is called
+            this.mainView.loadUrl("about:blank");
+
+            // Forward to plugins
+            if (this.mainView.pluginManager != null) {
+                this.mainView.pluginManager.onDestroy();
+            }
+        }
+        else {
+            //this.endActivity();
+        }
     }
+
+    
 
 }
